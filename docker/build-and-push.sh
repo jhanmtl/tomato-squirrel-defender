87d836e5c0973afea2b184ffa346cf08e825d109
jhanmtl <jing.han@mail.mcgill.ca>
2023-07-02 13:28:33 -0400

    establishing development environment using docker compose & multistage build

---
+++ docker/build-and-push.sh
@@ -0,0 +1,3 @@
+docker compose -f docker-compose.yaml build
+
+# todo: push to docker hub when ready
---
+++ docker/init-dev-environment.sh
@@ -0,0 +1,6 @@
+docker compose -f docker-compose.yaml run \
+    -it \
+    --rm \
+    --service-ports \
+    --name squirrel_defender_development \
+    squirrel_defender_development
